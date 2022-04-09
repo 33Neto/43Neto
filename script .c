97943143d4e0fc6662613ca9b6ce2d95d0845f44
33Neto <97692627+33Neto@users.noreply.github.com>
2022-04-08 21:39:51 -0400

    Update and rename README.md to script .c

---
+++ script .c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main()
+{
+    int contador;
+
+ printf ("QUAL SUA IDADE?\n");
+scanf ("%i",&contador);
+  
+  for (contador = 1; contador <= 99999;)
+  {
+    printf("Eneias GAY GAY\n");
+  }
+  
+  return(0);
+}
